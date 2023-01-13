c9654733bbc83a84c340a79400b589e7867efc82
Isha Kothadia <ikothadi@my.centennialcollege.ca>
2023-01-13 17:02:11 -0500

    first commit

---
+++ lib.js
@@ -0,0 +1,20 @@
+// Returns half of the given number
+export function half(x) {
+    return x / 2;
+}
+
+// Returns multiplication of 2 numbers
+export function multiply(x, y) {
+    return x * y;
+}
+
+// Returns string
+export function doSomething() {
+    return "Hey..!! Isha Kothadia here";
+}
+
+// Validators
+export let flag = false;
+export function touch() {
+flag = true;
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+// Import functions from lib
+import { half, multiply, doSomething, flag, touch } from './lib.js';
+console.log("Result after division is :", half(35));
+
+console.log("Multiplication answer is :",multiply(16, 4))
+
+console.log("String is :",doSomething())
+
+console.log(flag);
+touch();
+console.log(flag);
